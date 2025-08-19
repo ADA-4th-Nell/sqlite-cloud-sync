8cf0fdaa23202bf61b79f7313ea17cbccd6c98ad
nell <devstory.co.kr@gmail.com>
2025-08-19 14:04:32 +0900

    Initial Commit

---
+++ sqlite-cloud-sync/sqlite_cloud_syncApp.swift
@@ -0,0 +1,17 @@
+//
+//  sqlite_cloud_syncApp.swift
+//  sqlite-cloud-sync
+//
+//  Created by Nell on 8/19/25.
+//
+
+import SwiftUI
+
+@main
+struct sqlite_cloud_syncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
